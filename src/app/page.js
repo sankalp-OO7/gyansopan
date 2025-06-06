4c6091b7fbeb14d51ff9aa8842ee63bb1c45a9df
Sankalp <your.email@example.com>
2025-06-06 16:40:01 +0530

    finished

--- src/app/page.js
+++ src/app/page.js
@@ -143,7 +143,7 @@ export default function Home() {
               animate="animate"
               exit="exit"
             >
-              CHAINWORKS
+              GYANSOPAN
             </motion.span>
           </motion.div>
         )}
